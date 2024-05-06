865d6193d78cdc9dec46fd379c4fa5aaba52fc8c
Nakaoni <cho_tenro@hotmail.fr>
2024-05-06 22:27:35 +0200

    Fix HeapSort tests

--- tree/heap/__init__.py
+++ tree/heap/__init__.py
@@ -7,9 +7,9 @@ class Type(Enum):
 
 
 class Heap:
-    def __init__(self, type: Type):
+    def __init__(self, heap_type: Type):
         self.data = []
-        self.type = type
+        self.type = heap_type
 
     def __compare(self, a, b):
         if self.type == Type.Max:
@@ -35,16 +35,12 @@ class Heap:
         """
         formula = index * 2 + 1
         """
-        assert self.size() > 0
-
         return index * 2 + 1
 
     def right_child_index(self, index):
         """
         formula = index * 2 + 2
         """
-        assert self.size() > 0
-
         return index * 2 + 2
 
     def parent_index(self, index):
@@ -64,11 +60,7 @@ class Heap:
 
         # "Throttle up" to place the value at the correct index
         index = len(self.data) - 1
-        while (
-            index > 0
-            and index < self.size()
-            and self.__compare(self.data[index], self.data[self.parent_index(index)])
-        ):
+        while 0 < index < self.size() and self.__compare(self.data[index], self.data[self.parent_index(index)]):
             self.data[index], self.data[self.parent_index(index)] = (
                 self.data[self.parent_index(index)],
                 self.data[index],
@@ -82,7 +74,8 @@ class Heap:
         by set the last_node as the new root node
         Then move down the new root node to the correct place
         """
-        if self.size() <= 0:
+        if self.size() <= 1:
+            self.data.clear()
             return None
 
         self.data[0] = self.data.pop()
@@ -125,15 +118,17 @@ class Heap:
 
     def __get_better_child(self, trickle_index):
         if (
-            self.right_child_index(trickle_index) < self.size()
-            and not self.data[self.right_child_index(trickle_index)]
+                self.right_child_index(trickle_index) < self.size()
+                and not self.data[self.right_child_index(trickle_index)]
         ):
             return self.left_child_index(trickle_index)
 
-        if self.__compare(
-            self.data[self.right_child_index(trickle_index)],
-            self.data[self.left_child_index(trickle_index)],
-        ):
+        if (
+                self.right_child_index(trickle_index) < self.size() and
+                self.__compare(
+                self.data[self.right_child_index(trickle_index)],
+                self.data[self.left_child_index(trickle_index)],
+        )):
             return self.right_child_index(trickle_index)
         else:
             return self.left_child_index(trickle_index)
